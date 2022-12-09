400267cf4384bdf7b2f80ec0a05b6421c7ce8d79
racingf1 <racingf1@gmail.com>
2022-12-09 18:02:37 +0800

    feat: 準備推送上雲端

--- bootstore3/settings.py
+++ bootstore3/settings.py
@@ -38,8 +38,22 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookstore',
-    'corsheaders'
+    'corsheaders',
+    "bootstrap5",
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+# NPM_BIN_PATH="C:\\Program Files\\nodejs\\node_modules\\npm\\bin"
+# NPM_BIN_PATH = "/usr/local/bin/npm"
+NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
+# NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -51,6 +65,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'bootstore3.urls'
